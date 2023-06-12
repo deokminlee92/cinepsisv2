5f26ccee3d52299a2f8f43de586841a25ccd3044
deokminlee92 <deokminlee92@gmail.com>
2023-06-12 23:51:43 +0200

    [Around] CSS problem

--- src/components/Around/Around.js
+++ src/components/Around/Around.js
@@ -10,27 +10,23 @@ const Around = ({ arg }) => {
   const { data, isError, isLoading } = useGetLastestMoviesQuery(arg);
   console.log(data, isError, isLoading, "data, isError, isLoading");
 
-  return !isLoading ? (
-    <>
-      <section>
-        <Wrapper>
-          <S.Grid>
-            {data.map((movie) => {
-              const backdropUrl = `${API_IMG}${movie.backdrop_path}`;
-              return (
-                <div key={movie.id}>
-                  <img src={backdropUrl} alt="Backdrop" />
-                </div>
-              );
-            })}
-          </S.Grid>
-        </Wrapper>
-      </section>
-    </>
-  ) : (
-    <div>
-      <Loader />
-    </div>
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <Wrapper>
+      <S.Grid>
+        {data.map((movie) => {
+          const backdropUrl = `${API_IMG}${movie.backdrop_path}`;
+          return (
+            <S.ImageCard key={movie.id}>
+              <img src={backdropUrl} alt="Backdrop" />
+            </S.ImageCard>
+          );
+        })}
+      </S.Grid>
+    </Wrapper>
   );
 };
 
--- src/components/Around/Around.styled.js
+++ src/components/Around/Around.styled.js
@@ -4,30 +4,46 @@ const Grid = styled.div`
   display: flex;
   flex-flow: row wrap;
   justify-content: flex-start;
-  width: 500px;
-  height: 500px;
 `;
 
-const Card = styled.div`
-  position: relative;
-  flex-basis: 100%;
-  background: ${({ backdropPath }) => `url(${backdropPath}) center no-repeat`};
-  background-size: cover;
-  height: 30vh;
-  border-radius: 5px;
-  overflow: hidden;
-  margin-bottom: 3px;
-  box-shadow: 0 5px 3px 0 rgba(0, 0, 0, 0.3);
-  cursor: pointer;
+// const Card = styled.div`
+//   position: relative;
+//   flex-basis: 100%;
+//   background: ${({ backdropPath }) => `url(${backdropPath}) center no-repeat`};
+//   background-size: cover;
+//   height: 30vh;
+//   border-radius: 5px;
+//   overflow: hidden;
+//   margin-bottom: 3px;
+//   box-shadow: 0 5px 3px 0 rgba(0, 0, 0, 0.3);
+//   cursor: pointer;
 
-  &:after {
-    content: "";
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.3);
+//   &:after {
+//     content: "";
+//     position: absolute;
+//     left: 0;
+//     right: 0;
+//     top: 0;
+//     bottom: 0;
+//     background-color: rgba(0, 0, 0, 0.3);
+//   }
+// `;
+
+const ImageCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  height: 300px;
+  margin: 10px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 8px;
   }
 `;
-export { Grid, Card };
+export { Grid, ImageCard };
