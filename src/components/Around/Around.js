7917a8d7fd879310d46b80bdf9bdad51efd5ce60
deokminlee92 <deokminlee92@gmail.com>
2023-06-13 17:18:41 +0200

    [Around, Around.style] click imagebox -> detail -ing

--- src/components/Around/Around.js
+++ src/components/Around/Around.js
@@ -3,7 +3,8 @@ import { useGetLastestMoviesQuery } from "../../store/modules/fetch";
 import Loader from "../Loader/Loader";
 import * as S from "./Around.styled";
 import { Wrapper } from "../../style/variables";
-
+import StarRatings from "react-star-ratings";
+import { Link } from "react-router-dom";
 const API_IMG = "https://image.tmdb.org/t/p/original/";
 
 const Around = ({ arg }) => {
@@ -16,12 +17,29 @@ const Around = ({ arg }) => {
 
   return (
     <Wrapper>
+      <div></div>
       <S.Grid>
         {data.map((movie) => {
           const backdropUrl = `${API_IMG}${movie.backdrop_path}`;
           return (
             <S.ImageCard key={movie.id}>
               <img src={backdropUrl} alt="Backdrop" />
+              <S.Content>
+                <Link data-testid="item-link" to={`/detail/${movie.id}`}>
+                  <S.Title>{movie.title}</S.Title>
+                  <S.Container>
+                    <StarRatings
+                      rating={movie.vote_average / 2}
+                      starRatedColor="#ffda33"
+                      changeRating={movie.changeRating}
+                      numberOfStars={5}
+                      name="rating"
+                      starDimension="30px"
+                      starSpacing="2px"
+                    />
+                  </S.Container>
+                </Link>
+              </S.Content>
             </S.ImageCard>
           );
         })}
--- src/components/Around/Around.styled.js
+++ src/components/Around/Around.styled.js
@@ -4,19 +4,33 @@ const Grid = styled.div`
   display: flex;
   flex-flow: row wrap;
   justify-content: flex-start;
+  padding-top: 10%;
 `;
 
-// const Card = styled.div`
-//   position: relative;
-//   flex-basis: 100%;
-//   background: ${({ backdropPath }) => `url(${backdropPath}) center no-repeat`};
-//   background-size: cover;
-//   height: 30vh;
-//   border-radius: 5px;
-//   overflow: hidden;
-//   margin-bottom: 3px;
-//   box-shadow: 0 5px 3px 0 rgba(0, 0, 0, 0.3);
-//   cursor: pointer;
+const ImageCard = styled.div`
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  height: 300px;
+  margin: 15px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background: ${({ backdropPath }) => `url(${backdropPath}) center no-repeat`};
+  background-size: cover;
+  height: 30vh;
+  box-shadow: 0 5px 3px 0 rgba(0, 0, 0, 0.3);
+  /* cursor: pointer; */
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 8px;
+  }
+`;
 
 //   &:after {
 //     content: "";
@@ -28,22 +42,30 @@ const Grid = styled.div`
 //     background-color: rgba(0, 0, 0, 0.3);
 //   }
 // `;
+const Content = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
-const ImageCard = styled.div`
+const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 30%;
-  height: 300px;
-  margin: 10px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border-radius: 8px;
-  }
 `;
-export { Grid, ImageCard };
+
+const Title = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  font-size: 1.5em;
+  color: white;
+  font-weight: bold;
+  margin-bottom: 5px;
+  height: 100%;
+`;
+
+export { Grid, ImageCard, Content, Container, Title };
