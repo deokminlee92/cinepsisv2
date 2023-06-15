d5e84589bea4d397b4f3fdd0df77e1049f0450f6
deokminlee92 <deokminlee92@gmail.com>
2023-06-16 00:44:18 +0200

    [Around] still rendering issue

--- src/components/Around/Around.js
+++ src/components/Around/Around.js
@@ -5,11 +5,11 @@ import * as S from "./Around.styled";
 import { Wrapper } from "../../style/variables";
 import StarRatings from "react-star-ratings";
 import { Link } from "react-router-dom";
+
 const API_IMG = "https://image.tmdb.org/t/p/original/";
 
 const Around = ({ arg }) => {
   const { data, isError, isLoading } = useGetLastestMoviesQuery(arg);
-  console.log(data, isError, isLoading, "data, isError, isLoading");
 
   if (isLoading) {
     return <Loader />;
@@ -17,29 +17,30 @@ const Around = ({ arg }) => {
 
   return (
     <Wrapper>
-      <div></div>
       <S.Grid>
         {data.map((movie) => {
           const backdropUrl = `${API_IMG}${movie.backdrop_path}`;
           return (
             <S.ImageCard key={movie.id}>
-              <img src={backdropUrl} alt="Backdrop" />
-              <S.Content>
+              <S.ImageContainer>
                 <Link data-testid="item-link" to={`/detail/${movie.id}`}>
-                  <S.Title>{movie.title}</S.Title>
-                  <S.Container>
-                    <StarRatings
-                      rating={movie.vote_average / 2}
-                      starRatedColor="#ffda33"
-                      changeRating={movie.changeRating}
-                      numberOfStars={5}
-                      name="rating"
-                      starDimension="30px"
-                      starSpacing="2px"
-                    />
-                  </S.Container>
+                  <S.Image src={backdropUrl} alt="Backdrop" />
+                  <S.Content>
+                    <S.Title>{movie.title}</S.Title>
+                    <S.Rating>
+                      <StarRatings
+                        rating={movie.vote_average / 2}
+                        starRatedColor="#ffda33"
+                        changeRating={movie.changeRating}
+                        numberOfStars={5}
+                        name="rating"
+                        starDimension="30px"
+                        starSpacing="2px"
+                      />
+                    </S.Rating>
+                  </S.Content>
                 </Link>
-              </S.Content>
+              </S.ImageContainer>
             </S.ImageCard>
           );
         })}
--- src/components/Around/Around.styled.js
+++ src/components/Around/Around.styled.js
@@ -10,62 +10,47 @@ const Grid = styled.div`
 const ImageCard = styled.div`
   position: relative;
   overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   width: 30%;
-  height: 300px;
   margin: 15px;
   border: 1px solid #ccc;
   border-radius: 8px;
-  background: ${({ backdropPath }) => `url(${backdropPath}) center no-repeat`};
-  background-size: cover;
-  height: 30vh;
   box-shadow: 0 5px 3px 0 rgba(0, 0, 0, 0.3);
-  /* cursor: pointer; */
-
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border-radius: 8px;
-  }
 `;
 
-//   &:after {
-//     content: "";
-//     position: absolute;
-//     left: 0;
-//     right: 0;
-//     top: 0;
-//     bottom: 0;
-//     background-color: rgba(0, 0, 0, 0.3);
-//   }
-// `;
-const Content = styled.div`
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.span`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  font-size: 1.5em;
-  color: white;
-  font-weight: bold;
-  margin-bottom: 5px;
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
   height: 100%;
 `;
 
-export { Grid, ImageCard, Content, Container, Title };
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 8px;
+`;
+
+const Content = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: white;
+`;
+
+const Title = styled.h3`
+  margin: 0;
+  color: white;
+  font-size: 16px;
+  text-align: center;
+`;
+
+const Rating = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 5px;
+`;
+
+export { Grid, ImageCard, Image, Content, ImageContainer, Rating, Title };
